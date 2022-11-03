f788ba0c1ccabc531fe3a271856d44024171c68e
rahulc321 <rahulidcsoftwares@gmail.com>
2022-11-03 21:14:23 +0530

    changes

--- termsconditions.php
+++ termsconditions.php
@@ -11,7 +11,7 @@
           <ol class="breadcrumb mb-0">
             <li class="breadcrumb-item"><a href="index.html">Home</a></li>
             
-            <li class="breadcrumb-item active" aria-current="page">Terms and Conditions</li>
+            <li class="breadcrumb-item active" aria-current="page">Terms and Conditions  </li>
           </ol>
         </nav>
       </div>
